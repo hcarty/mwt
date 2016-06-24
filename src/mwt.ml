6271943bf2c833537dbb78d08edf120a64de9e2d
Hezekiah M. Carty <hez@0ok.org>
2016-06-24 16:57:38 -0400

    Initial commit

---
+++ src/mwt.ml
@@ -0,0 +1,173 @@
+(* Mwt, derived from the Lwt project's lwt_preemptive.ml
+ * Copyright (C) 2005 Nataliya Guts, Vincent Balat, Jérôme Vouillon
+ * Laboratoire PPS - CNRS Université Paris Diderot
+ *               2009 Jérémie Dimino
+ *               2016 Hezekiah Carty (Mwt modifications of Lwt_preemptive)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, with linking exceptions;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * See COPYING file for details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*)
+
+open Lwt.Infix
+
+type worker = {
+  task_channel: (int * (unit -> unit)) option Event.channel;
+  (* Channel used to communicate notification id and tasks to the
+     worker thread. *)
+
+  mutable thread : Thread.t; (* The worker thread. *)
+  pool : t; (* The worker's parent thread pool *)
+}
+and t = {
+  num_threads : int; (* Number of preemptive threads in the pool *)
+  workers : worker Queue.t; (* Queue of worker threads *)
+  waiters : worker Lwt.u Lwt_sequence.t; (* Queue of clients waiting for a
+                                            worker to be available *)
+}
+
+(* Code executed by a worker: *)
+let worker_loop ?init ?at_exit worker =
+  begin
+    match init with
+    | None -> ()
+    | Some f -> f ()
+  end;
+  let continue = ref true in
+  while !continue do
+    match Event.sync (Event.receive worker.task_channel) with
+    | Some (id, task) ->
+      task ();
+      (* Tell the main thread that work is done: *)
+      Lwt_unix.send_notification id
+    | None -> continue := false
+  done;
+  begin
+    match at_exit with
+    | None -> ()
+    | Some f -> f ()
+  end
+
+(* create a new worker: *)
+let make_worker ?init ?at_exit pool =
+  let worker =
+    {
+      task_channel = Event.new_channel ();
+      thread = Thread.self ();
+      pool;
+    }
+  in
+  worker.thread <- Thread.create (worker_loop ?init ?at_exit) worker;
+  worker
+
+(* Add a worker to the pool *)
+let add_worker pool worker =
+  match Lwt_sequence.take_opt_l pool.waiters with
+  | None ->
+    Queue.add worker pool.workers
+  | Some w ->
+    Lwt.wakeup w worker
+
+(* Wait for worker to be available, then return it: *)
+let rec get_worker pool =
+  if not (Queue.is_empty pool.workers) then
+    Lwt.return (Queue.take pool.workers)
+  else
+    Lwt.add_task_r pool.waiters
+
+(*
+let cleanup pool =
+  failwith "TODO"
+*)
+
+let make ?init ?at_exit num_threads =
+  let pool =
+    { num_threads; workers = Queue.create (); waiters = Lwt_sequence.create () }
+  in
+  for _i = 1 to num_threads do
+    Queue.add (make_worker ?init ?at_exit pool) pool.workers
+  done;
+(*   Gc.finalise cleanup pool; *)
+  pool
+
+let detach pool f args =
+  let result = ref (Lwt.make_error (Failure "Mwt.detach")) in
+  (* The task for the worker thread: *)
+  let task () =
+    try
+      result := Lwt.make_value (f args)
+    with exn ->
+      result := Lwt.make_error exn
+  in
+  get_worker pool >>= fun worker ->
+  let waiter, wakener = Lwt.wait () in
+  let id =
+    Lwt_unix.make_notification ~once:true
+      (fun () -> Lwt.wakeup_result wakener !result)
+  in
+  Lwt.finalize
+    (fun () ->
+       (* Send the id and the task to the worker: *)
+       Event.sync (Event.send worker.task_channel (Some (id, task)));
+       waiter)
+    (fun () ->
+       add_worker pool worker;
+       Lwt.return_unit)
+
+(* +-----------------------------------------------------------------+
+   | Running Lwt threads in the main thread                          |
+   +-----------------------------------------------------------------+ *)
+
+type 'a result =
+  | Value of 'a
+  | Error of exn
+
+(* Queue of [unit -> unit Lwt.t] functions. *)
+let jobs = Queue.create ()
+
+(* Mutex to protect access to [jobs]. *)
+let jobs_mutex = Mutex.create ()
+
+let job_notification =
+  Lwt_unix.make_notification
+    (fun () ->
+       (* Take the first job. The queue is never empty at this
+          point. *)
+       Mutex.lock jobs_mutex;
+       let thunk = Queue.take jobs in
+       Mutex.unlock jobs_mutex;
+       ignore (thunk ()))
+
+let run_in_main f =
+  let channel = Event.new_channel () in
+  (* Create the job. *)
+  let job () =
+    (* Execute [f] and wait for its result. *)
+    Lwt.try_bind f
+      (fun ret -> Lwt.return (Value ret))
+      (fun exn -> Lwt.return (Error exn)) >>= fun result ->
+    (* Send the result. *)
+    Event.sync (Event.send channel result);
+    Lwt.return_unit
+  in
+  (* Add the job to the queue. *)
+  Mutex.lock jobs_mutex;
+  Queue.add job jobs;
+  Mutex.unlock jobs_mutex;
+  (* Notify the main thread. *)
+  Lwt_unix.send_notification job_notification;
+  (* Wait for the result. *)
+  match Event.sync (Event.receive channel) with
+  | Value ret -> ret
+  | Error exn -> raise exn
---
+++ src/mwt.mli
@@ -0,0 +1,16 @@
+type t
+(** Preemptive thread pool *)
+
+val make : ?init:(unit -> unit) -> ?at_exit:(unit -> unit) -> int -> t
+(** [make ?max_threads_queued ?init ?at_exit num_threads] creates a new pool
+    of [num_threads] preemptive threads.
+
+    @param init is run when each thread in the pool is created
+    @param at_exit is run when each thread in the pool exits *)
+
+val detach : t -> ('a -> 'b) -> 'a -> 'b Lwt.t
+(** [detach pool f x] will run [f x] in one of the preemptive threads in
+    [pool]. *)
+
+val run_in_main : (unit -> 'a Lwt.t) -> 'a
+(** [run_in_main f x] can be used to run [f x] in the program's main thread. *)
