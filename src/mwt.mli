7dde3dcc05c1cd8d4ae8cb7127ae449e4a69a0ae
Hezekiah M. Carty <hez@0ok.org>
2016-06-27 15:05:02 -0400

    Add Mwt.run; Move pool-related code into Pool submodule

--- src/mwt.mli
+++ src/mwt.mli
@@ -1,16 +1,30 @@
-type t
-(** Preemptive thread pool *)
+(** {1 Preemptive threads in Lwt sauce} *)
 
-val make : ?init:(unit -> unit) -> ?at_exit:(unit -> unit) -> int -> t
-(** [make ?max_threads_queued ?init ?at_exit num_threads] creates a new pool
-    of [num_threads] preemptive threads.
+val run :
+  ?init:(unit -> unit) ->
+  ?at_exit:(unit -> unit) ->
+  ('a -> 'b) -> 'a -> 'b Lwt.t
+(** [run f x] will run [f x] in a freshly created preemptive thread.  The
+    preemptive thread will end when [f x] returns. *)
 
-    @param init is run when each thread in the pool is created
-    @param at_exit is run when each thread in the pool exits *)
+module Pool : sig
+  (** {2 Preemptive thread pools} *)
 
-val detach : t -> ('a -> 'b) -> 'a -> 'b Lwt.t
-(** [detach pool f x] will run [f x] in one of the preemptive threads in
-    [pool]. *)
+  type t
+  (** Preemptive thread pool *)
 
-val run_in_main : (unit -> 'a Lwt.t) -> 'a
-(** [run_in_main f x] can be used to run [f x] in the program's main thread. *)
+  val make : ?init:(unit -> unit) -> ?at_exit:(unit -> unit) -> int -> t
+  (** [make ?max_threads_queued ?init ?at_exit num_threads] creates a new pool
+      of [num_threads] preemptive threads.
+
+      @param init is run when each thread in the pool is created
+      @param at_exit is run when each thread in the pool exits *)
+
+  val detach : t -> ('a -> 'b) -> 'a -> 'b Lwt.t
+  (** [detach pool f x] will run [f x] in one of the preemptive threads in
+      [pool]. *)
+
+  val run_in_main : (unit -> 'a Lwt.t) -> 'a
+  (** [run_in_main f x] can be used to run [f x] in the program's main
+      thread. *)
+end
